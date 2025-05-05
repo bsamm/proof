09807f86032025ffea630b67c86bb4d6bc2c4c50
bsamm <brandonc.sammons@gmail.com>
2025-05-04 23:43:48 -0500

    remove migrations from the build step

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -20,4 +20,5 @@ bin/rails assets:clean
 
 # -- Database Migrations --
 # Consider moving this to a pre-deploy command on Render for zero-downtime deploys
-bin/rails db:migrate
+# moved here: https://dashboard.render.com/web/srv-d0a2e995pdvs73bjup9g/settings
+# bin/rails db:migrate
